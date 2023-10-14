1c98543208fe71d9ec075ee25b69281a9970bb04
mudassar325 <118302723+mudassar325@users.noreply.github.com>
2023-10-14 16:26:04 +0500

    Stable Diffusion XL FREE VERSION
    Stable Diffusion XL FREE VERSION 
    Modified by Mudassar Gujjar (IG-@MGX169)
    Original : https://github.com/AUTOMATIC1111/stable-diffusion-webui THANKS To Automatic 1111 And Camenduru

---
+++ README.md
@@ -0,0 +1,6 @@
+## Main Repo
+https://github.com/AUTOMATIC1111/stable-diffusion-webui (Thanks to @AUTOMATIC1111 ❤)
+Modified by Mudassar Gujjar (MGX169 )
+IG@Mgx169
+
+
---
+++ header.py
@@ -0,0 +1,8 @@
+        with gr.Group():
+            gr.Markdown(f'''
+              THIS COUSTOMIZED VERSION OF STABLE DIFUSSION -XL 1.0
+              MODIEY BY Mudassar Gujjar (Mgx169)
+              INSTGRAM :@Mgx169
+              https://github.com/AUTOMATIC1111/stable-diffusion-webui (Thanks to @AUTOMATIC1111 and Camenduru ❤)
+            ''')
+
